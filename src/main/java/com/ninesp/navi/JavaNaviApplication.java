56207333b3ef906980677ccbd16ecabf7c112ff9
chenjun <darmor@qq.com>
2022-08-16 11:36:15 +0800

    first commit

---
+++ src/main/java/com/ninesp/navi/JavaNaviApplication.java
@@ -0,0 +1,13 @@
+package com.ninesp.navi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaNaviApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaNaviApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ninesp/navi/JavaNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ninesp.navi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaNaviApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
